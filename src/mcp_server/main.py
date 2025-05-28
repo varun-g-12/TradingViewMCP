c07d9045333f9ca0bbc0ed87ee99e9fb1c761056
Varun G <varungangu1@gmail.com>
2025-05-28 06:33:22 +0000

    feat: Move constants to a separate file for better organization and maintainability

--- src/mcp_server/main.py
+++ src/mcp_server/main.py
@@ -4,39 +4,7 @@ from typing import Any, Dict, List, Literal
 import pandas as pd
 import requests
 from mcp.server.fastmcp import FastMCP
-
-HEADERS: Dict[str, str] = {
-    "accept": "application/json",
-    "accept-language": "en-IN,en-GB;q=0.9,en;q=0.8,en-US;q=0.7",
-    "content-type": "text/plain;charset=UTF-8",
-    "origin": "https://in.tradingview.com",
-    "referer": "https://in.tradingview.com/",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0",
-}
-
-PARAMS: Dict[str, str] = {
-    "label-product": "screener-stock",
-}
-
-COLUMNS: List[str] = [
-    "name",
-    "close",
-    "change",
-    "volume",
-    "relative_volume_10d_calc",
-    "Recommend.All",
-]
-
-SYMBOLS: List[str] = ["SYML:NSE;NIFTY", "SYML:NSE;NIFTYJR"]
-
-MARKETS: List[str] = ["india"]
-
-DATA: Dict[str, Any] = {
-    "columns": COLUMNS,
-    "sort": {"sortBy": "market_cap_basic", "sortOrder": "desc"},
-    "symbols": {"symbolset": SYMBOLS},
-    "markets": MARKETS,
-}
+from mcp_server.constants import HEADERS, PARAMS, COLUMNS, SYMBOLS, MARKETS, DATA
 
 # Create an MCP server
 mcp = FastMCP("Tradingview_mcp")
